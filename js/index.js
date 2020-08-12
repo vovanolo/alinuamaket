270d28551776321f6b03be19150f1cc6a9442701
vovanolo <vovchik.student221@gmail.com>
2020-08-12 14:57:57 +0300

    header

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay:{
+        delay: 5000
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+  
+    // And if we need scrollbar
+  })
