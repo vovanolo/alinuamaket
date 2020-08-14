a54d88b1e7181fc56f4a6835e9a38d13cf97ad93
vovanolo <vovchik.student221@gmail.com>
2020-08-14 14:56:55 +0300

    the news and how it works section

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay:{
-        delay: 5000
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-  
-    // And if we need scrollbar
-  })
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay:{
+      delay: 5000
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+
+  // And if we need scrollbar
+});
+
+const navbar = document.querySelector('.navbar');
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+window.addEventListener('scroll', (e) => {
+  const scrollY = e.currentTarget.scrollY;
+  if (scrollY > 100) {
+    navbar.classList.add('bg-light');
+  }
+  else {
+    navbar.classList.remove('bg-light');
+  }
+});
+
+$('#navbarNavDropdown').on('show.bs.collapse', () => {
+  navbar.classList.add('bg-light');
+});
+
+$('#navbarNavDropdown').on('hide.bs.collapse', () => {
+  navbar.classList.remove('bg-light');
+});
