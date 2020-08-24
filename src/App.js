b71465d27c0511e64289c21bc87c107488dcb47b
olehhapuk <oleggapuk20@gmail.com>
2020-08-24 11:26:08 +0300

    feat: added translations for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = useState('ua');
+
+  function changeLanguage(e) {
+    const newLanguage = e.currentTarget.checked ? 'en' : 'ua';
+    i18n.changeLanguage(newLanguage);
+    setLanguage(newLanguage);
+  }
+
   return (
-    <h1>Hello, World!</h1>
+    <div>
+      <h1>{t('Hello, World')}</h1>
+      <input type="checkbox" onChange={changeLanguage} />
+      <p>Current language is {language}</p>
+    </div>
   );
 }
 
